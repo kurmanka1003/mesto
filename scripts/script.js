8a5a9427c4d15ac6cf8b1b221ed77b9723fa78aa
Rumiya Kurmangazieva <kurmangazieva_2001@mail.ru>
2023-01-13 10:18:19 +0300

    refactor: изменить название переменной и функцию

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,14 @@
 const editButton = document.querySelector('.profile__edit-button');
-const aPopup = document.querySelector('.popup');
+const popupProfile = document.querySelector('.popup');
 const closeButton = document.querySelector('.popup__close');
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
 const formElement = document.querySelector('[name="info"]');
-const nameInput = aPopup.querySelector('[name="name"]');
-const jobInput = aPopup.querySelector('[name="job"]');
+const nameInput = popupProfile.querySelector('[name="name"]');
+const jobInput = popupProfile.querySelector('[name="job"]');
 
 editButton.addEventListener('click', (evt) => {
-    aPopup.classList.add('popup_opened');
+    popupProfile.classList.add('popup_opened');
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;
 })
@@ -18,14 +18,14 @@ function closePopup(popup) {
 }
 
 closeButton.addEventListener('click', (evt) => {
-    aPopup.classList.remove('popup_opened');
+    closePopup(popupProfile);
 })
 
 function handleFormSubmit(evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
-    closePopup(aPopup);
+    closePopup(popupProfile);
 }
 
-aPopup.addEventListener('submit', handleFormSubmit); 
+popupProfile.addEventListener('submit', handleFormSubmit); 
